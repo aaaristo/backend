a4ed2d74243479c93311e8758c2c2981df319907
Andrea Amerigo Aristodemo Gariboldi <andrea.gariboldi@gmail.com>
2014-02-27 13:35:23 +0100

    initial

---
+++ js/app.js
@@ -0,0 +1,23 @@
+angular.module('backend', ['ngRoute','pouchdb','angular-growl'])
+       .config(['$routeProvider','growlProvider',
+       function ($routeProvider,growlProvider)
+       {
+          $routeProvider
+              .when('/home', {controller: _home, templateUrl:'/views/home.html' })
+              .when('/thing/:id', { controller: _thing, templateUrl:'/views/thing.html' })
+              .otherwise({redirectTo:'/home'});
+
+          growlProvider.globalTimeToLive(5000);
+          growlProvider.onlyUniqueMessages(false);
+       }])
+       .directive('bsHolder',function ()
+       {
+          return {
+            link: function (scope, element, attrs) {
+                Holder.run({ images: element.get(0) });
+            }
+          };
+       })
+       .factory('db', ['pouchdb',function(pouchdb) {
+          return pouchdb.create('thingsdb');
+       }]);
---
+++ js/ctrl/thing.js
@@ -0,0 +1,45 @@
+var _thing= ['$scope','$routeParams','$location','db','growl',
+function ($scope,$routeParams,$location,db,growl)
+{
+   $('#name').focus(); 
+
+   $('#fields').keypress(function(e)
+   {
+      if (e.which == 13)
+        $scope.$apply($scope.createFields);
+   });
+
+   $scope.fields= [];
+   $scope.thing= { _id: $routeParams.id };
+
+   $scope.createFields= function ()
+   {
+      var fields= $scope.fieldNames.split(/\s+/);
+
+      $scope.fields= _.filter($scope.fields,function (f) { return _.contains(fields,f.name); });
+
+      _(fields).each(function (name)
+      {
+          if (!_.contains(_.pluck($scope.fields,'name'),name))
+            $scope.fields.push({ name: name,
+                                 type: 'text',
+                                label: name.substring(0,1).toUpperCase()
+                                      +name.substring(1).toLowerCase() });
+      });
+   };
+
+   $scope.save= function ()
+   {
+      db.put($scope.thing,
+      function (err, response)
+      {
+        if (err)
+          growl.addErrorMessage('woops, cannot save this thing: '+err.message);
+        else
+        {
+          growl.addSuccessMessage($scope.thing.name+' saved!');
+          $location.path('things');
+        }
+      });
+   };
+}];
